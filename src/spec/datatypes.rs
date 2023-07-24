debda0deef99155a23624d8959b3758faa59c9c1
Jan Kaul <jankaul@mailbox.org>
2023-07-24 09:27:13 +0200

    fix display structtype

--- src/spec/datatypes.rs
+++ src/spec/datatypes.rs
@@ -43,7 +43,7 @@ impl fmt::Display for Type {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             Type::Primitive(primitive) => write!(f, "{}", primitive),
-            Type::Struct(_) => write!(f, "struct"),
+            Type::Struct(s) => write!(f, "{}", s),
             Type::List(_) => write!(f, "list"),
             Type::Map(_) => write!(f, "map"),
         }
