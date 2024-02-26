456fad4bd60f5ff1a1534b5d65fa414d144c21af
Chengxu Bian <piensengxv@gmail.com>
2024-02-25 19:13:36 -0500

    add license

--- crates/iceberg/tests/file_io_s3_test.rs
+++ crates/iceberg/tests/file_io_s3_test.rs
@@ -1,3 +1,22 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+//! Integration tests for FileIO S3.
+
 use futures::{AsyncReadExt, AsyncWriteExt};
 use iceberg::io::{
     FileIO, FileIOBuilder, S3_ACCESS_KEY_ID, S3_ENDPOINT, S3_REGION, S3_SECRET_ACCESS_KEY,
@@ -22,7 +41,7 @@ impl MinIOFixture {
         let read_port = format!("{}:{}", container_ip, 9000);
         loop {
             if !scan_port_addr(&read_port) {
-                log::info!("Waiting for 1s rest catalog to ready...");
+                log::info!("Waiting 1s for MinIO to ready...");
                 sleep(std::time::Duration::from_millis(1000)).await;
             } else {
                 break;
