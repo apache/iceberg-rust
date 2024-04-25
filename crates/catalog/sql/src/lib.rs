0b1141cc60ebd498a806dbc558e3c99b8def8b0d
JanKaul <jankaul@mailbox.org>
2024-04-25 09:41:57 +0200

    Fix heading
    Co-authored-by: Renjie Liu <liurenjie2008@gmail.com>

--- crates/catalog/sql/src/lib.rs
+++ crates/catalog/sql/src/lib.rs
@@ -15,7 +15,7 @@
 // specific language governing permissions and limitations
 // under the License.
 
-//! Iceberg REST API implementation.
+//! Iceberg sql catalog implementation.
 
 #![deny(missing_docs)]
 
