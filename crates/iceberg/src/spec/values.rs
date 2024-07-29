c81dd71df19e65d39c46fe240f524ee1de0a0093
ZENOTME <43447882+ZENOTME@users.noreply.github.com>
2024-07-29 22:08:22 +0800

    add check compatible func for primitive type (#492)
    Co-authored-by: ZENOTME <st810918843@gmail.com>

--- crates/iceberg/src/spec/values.rs
+++ crates/iceberg/src/spec/values.rs
@@ -76,7 +76,7 @@ pub enum PrimitiveLiteral {
     /// UTF-8 bytes (without length)
     String(String),
     /// 16-byte big-endian value
-    UUID(Uuid),
+    Uuid(Uuid),
     /// Binary value
     Fixed(Vec<u8>),
     /// Binary value (without length)
@@ -278,8 +278,8 @@ impl PartialOrd for Datum {
                 PrimitiveType::String,
             ) => val.partial_cmp(other_val),
             (
-                PrimitiveLiteral::UUID(val),
-                PrimitiveLiteral::UUID(other_val),
+                PrimitiveLiteral::Uuid(val),
+                PrimitiveLiteral::Uuid(other_val),
                 PrimitiveType::Uuid,
                 PrimitiveType::Uuid,
             ) => val.partial_cmp(other_val),
@@ -333,7 +333,7 @@ impl Display for Datum {
                 write!(f, "{}", microseconds_to_datetimetz(*val))
             }
             (_, PrimitiveLiteral::String(val)) => write!(f, r#""{}""#, val),
-            (_, PrimitiveLiteral::UUID(val)) => write!(f, "{}", val),
+            (_, PrimitiveLiteral::Uuid(val)) => write!(f, "{}", val),
             (_, PrimitiveLiteral::Fixed(val)) => display_bytes(val, f),
             (_, PrimitiveLiteral::Binary(val)) => display_bytes(val, f),
             (
@@ -410,7 +410,7 @@ impl Datum {
                 PrimitiveLiteral::String(std::str::from_utf8(bytes)?.to_string())
             }
             PrimitiveType::Uuid => {
-                PrimitiveLiteral::UUID(Uuid::from_u128(u128::from_be_bytes(bytes.try_into()?)))
+                PrimitiveLiteral::Uuid(Uuid::from_u128(u128::from_be_bytes(bytes.try_into()?)))
             }
             PrimitiveType::Fixed(_) => PrimitiveLiteral::Fixed(Vec::from(bytes)),
             PrimitiveType::Binary => PrimitiveLiteral::Binary(Vec::from(bytes)),
@@ -443,7 +443,7 @@ impl Datum {
             PrimitiveLiteral::Timestamp(val) => ByteBuf::from(val.to_le_bytes()),
             PrimitiveLiteral::Timestamptz(val) => ByteBuf::from(val.to_le_bytes()),
             PrimitiveLiteral::String(val) => ByteBuf::from(val.as_bytes()),
-            PrimitiveLiteral::UUID(val) => ByteBuf::from(val.as_u128().to_be_bytes()),
+            PrimitiveLiteral::Uuid(val) => ByteBuf::from(val.as_u128().to_be_bytes()),
             PrimitiveLiteral::Fixed(val) => ByteBuf::from(val.as_slice()),
             PrimitiveLiteral::Binary(val) => ByteBuf::from(val.as_slice()),
             PrimitiveLiteral::Decimal(_) => todo!(),
@@ -868,7 +868,7 @@ impl Datum {
     pub fn uuid(uuid: Uuid) -> Self {
         Self {
             r#type: PrimitiveType::Uuid,
-            literal: PrimitiveLiteral::UUID(uuid),
+            literal: PrimitiveLiteral::Uuid(uuid),
         }
     }
 
@@ -1408,7 +1408,7 @@ impl Literal {
 
     /// Creates uuid literal.
     pub fn uuid(uuid: Uuid) -> Self {
-        Self::Primitive(PrimitiveLiteral::UUID(uuid))
+        Self::Primitive(PrimitiveLiteral::Uuid(uuid))
     }
 
     /// Creates uuid from str. See [`Uuid::parse_str`].
@@ -1655,7 +1655,7 @@ impl Literal {
                     Ok(Some(Literal::Primitive(PrimitiveLiteral::String(s))))
                 }
                 (PrimitiveType::Uuid, JsonValue::String(s)) => Ok(Some(Literal::Primitive(
-                    PrimitiveLiteral::UUID(Uuid::parse_str(&s)?),
+                    PrimitiveLiteral::Uuid(Uuid::parse_str(&s)?),
                 ))),
                 (PrimitiveType::Fixed(_), JsonValue::String(_)) => todo!(),
                 (PrimitiveType::Binary, JsonValue::String(_)) => todo!(),
@@ -1793,7 +1793,7 @@ impl Literal {
                         .to_string(),
                 )),
                 PrimitiveLiteral::String(val) => Ok(JsonValue::String(val.clone())),
-                PrimitiveLiteral::UUID(val) => Ok(JsonValue::String(val.to_string())),
+                PrimitiveLiteral::Uuid(val) => Ok(JsonValue::String(val.to_string())),
                 PrimitiveLiteral::Fixed(val) => Ok(JsonValue::String(val.iter().fold(
                     String::new(),
                     |mut acc, x| {
@@ -1882,7 +1882,7 @@ impl Literal {
                 PrimitiveLiteral::Fixed(any) => Box::new(any),
                 PrimitiveLiteral::Binary(any) => Box::new(any),
                 PrimitiveLiteral::String(any) => Box::new(any),
-                PrimitiveLiteral::UUID(any) => Box::new(any),
+                PrimitiveLiteral::Uuid(any) => Box::new(any),
                 PrimitiveLiteral::Decimal(any) => Box::new(any),
             },
             _ => unimplemented!(),
@@ -2189,7 +2189,7 @@ mod _serde {
                     super::PrimitiveLiteral::Timestamp(v) => RawLiteralEnum::Long(v),
                     super::PrimitiveLiteral::Timestamptz(v) => RawLiteralEnum::Long(v),
                     super::PrimitiveLiteral::String(v) => RawLiteralEnum::String(v),
-                    super::PrimitiveLiteral::UUID(v) => {
+                    super::PrimitiveLiteral::Uuid(v) => {
                         RawLiteralEnum::Bytes(ByteBuf::from(v.as_u128().to_be_bytes()))
                     }
                     super::PrimitiveLiteral::Fixed(v) => RawLiteralEnum::Bytes(ByteBuf::from(v)),
@@ -2818,7 +2818,7 @@ mod tests {
 
         check_json_serde(
             record,
-            Literal::Primitive(PrimitiveLiteral::UUID(
+            Literal::Primitive(PrimitiveLiteral::Uuid(
                 Uuid::parse_str("f79c3e09-677c-4bbd-a479-3f349cb785e7").unwrap(),
             )),
             &Type::Primitive(PrimitiveType::Uuid),
