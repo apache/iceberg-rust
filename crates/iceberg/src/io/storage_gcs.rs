0900c152b6b7a07d32c40a235ddab1b93bf66f6a
Jack Dockerty <jdockerty19@gmail.com>
2024-08-05 17:09:40 +0100

    fix: do not introduce new properties

--- crates/iceberg/src/io/storage_gcs.rs
+++ crates/iceberg/src/io/storage_gcs.rs
@@ -25,44 +25,17 @@ use crate::{Error, ErrorKind, Result};
 
 // Reference: https://github.com/apache/iceberg/blob/main/gcp/src/main/java/org/apache/iceberg/gcp/GCPProperties.java
 
-/// Google Cloud Storage bucket name
-pub const GCS_BUCKET: &str = "gcs.bucket";
+/// Google Cloud Project ID
+pub const GCS_PROJECT_ID: &str = "gcs.project-id";
 /// Google Cloud Storage endpoint
-pub const GCS_ENDPOINT: &str = "gcs.endpoint";
-/// Google Cloud Storage OAuth token
-pub const GCS_OAUTH2_TOKEN: &str = "gcs.oauth2.token";
-/// Google Cloud Storage working (root) directory
-pub const GCS_ROOT: &str = "gcs.root";
-/// Google Cloud Storage working (root) directory
-pub const GCS_CREDENTIAL_PATH: &str = "gcs.credential-path";
+pub const GCS_SERVICE_PATH: &str = "gcs.service.path";
 
 /// Parse iceberg properties to [`GcsConfig`].
 pub(crate) fn gcs_config_parse(mut m: HashMap<String, String>) -> Result<GcsConfig> {
     let mut cfg = GcsConfig::default();
 
-    if let Some(bucket) = m.remove(GCS_BUCKET) {
-        cfg.bucket = bucket;
-    } else {
-        return Err(Error::new(
-            ErrorKind::DataInvalid,
-            "Bucket name is required for GCS",
-        ));
-    }
-
-    if let Some(root) = m.remove(GCS_ROOT) {
-        cfg.root = Some(root)
-    }
-
-    if let Some(endpoint) = m.remove(GCS_ENDPOINT) {
+    if let Some(endpoint) = m.remove(GCS_SERVICE_PATH) {
         cfg.endpoint = Some(endpoint);
-    }
-
-    if let Some(cred_path) = m.remove(GCS_CREDENTIAL_PATH) {
-        cfg.credential_path = Some(cred_path);
-    }
-
-    if let Some(token) = m.remove(GCS_OAUTH2_TOKEN) {
-        cfg.credential = Some(token);
     }
 
     Ok(cfg)
