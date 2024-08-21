6e1b411eb8f891a08cc65520dbbbda64457eb4f7
sungwy <sungwyun8@gmail.com>
2024-08-21 13:58:26 -0400

    adopt review comment

--- bindings/python/src/transform.rs
+++ bindings/python/src/transform.rs
@@ -15,10 +15,8 @@
 // specific language governing permissions and limitations
 // under the License.
 
-use std::{error, fmt};
 use iceberg::spec::Transform;
 use iceberg::transform::create_transform_function;
-use iceberg::Error;
 
 use arrow::{
     array::{make_array, Array, ArrayData},
@@ -26,40 +24,8 @@ use arrow::{
 use arrow::pyarrow::{FromPyArrow, ToPyArrow};
 use pyo3::{exceptions::PyValueError, prelude::*};
 
-#[derive(Debug)]
-enum PyO3IcebergError {
-    Error(Error),
-}
-
-impl fmt::Display for PyO3IcebergError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match *self {
-            PyO3IcebergError::Error(ref e) => e.fmt(f),
-        }
-    }
-}
-
-impl error::Error for PyO3IcebergError {
-    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
-        match *self {
-            // The cause is the underlying implementation error type. Is implicitly
-            // cast to the trait object `&error::Error`. This works because the
-            // underlying type already implements the `Error` trait.
-            PyO3IcebergError::Error(ref e) => Some(e),
-        }
-    }
-}
-
-impl From<Error> for PyO3IcebergError {
-    fn from(err: Error) -> PyO3IcebergError {
-        PyO3IcebergError::Error(err)
-    }
-}
-
-impl From<PyO3IcebergError> for PyErr {
-    fn from(err: PyO3IcebergError) -> PyErr {
-        PyValueError::new_err(err.to_string())
-    }
+fn to_py_err(err: iceberg::Error) -> PyErr {
+    PyValueError::new_err(err.to_string())
 }
 
 #[pyfunction]
@@ -67,8 +33,8 @@ pub fn bucket_transform(array: PyObject, num_buckets: u32, py: Python) -> PyResu
     // import
     let array = ArrayData::from_pyarrow_bound(array.bind(py))?;
     let array = make_array(array);
-    let bucket = create_transform_function(&Transform::Bucket(num_buckets)).map_err(PyO3IcebergError::from)?;
-    let array = bucket.transform(array).map_err(PyO3IcebergError::from)?;
+    let bucket = create_transform_function(&Transform::Bucket(num_buckets)).map_err(to_py_err)?;
+    let array = bucket.transform(array).map_err(to_py_err)?;
     // export
     let array = array.into_data();
     array.to_pyarrow(py)
