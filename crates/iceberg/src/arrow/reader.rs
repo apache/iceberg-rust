4c21f001789ceecd7f589d357de669154234641f
Scott Donnelly <scott@donnel.ly>
2024-09-25 07:57:28 +0100

    feat: add delete_file_index and populate in table scan

--- crates/iceberg/src/arrow/reader.rs
+++ crates/iceberg/src/arrow/reader.rs
@@ -49,8 +49,12 @@ use crate::expr::visitors::row_group_metrics_evaluator::RowGroupMetricsEvaluator
 use crate::expr::{BoundPredicate, BoundReference};
 use crate::io::{FileIO, FileMetadata, FileRead};
 use crate::runtime::spawn;
-use crate::scan::{ArrowRecordBatchStream, FileScanTask, FileScanTaskStream};
-use crate::spec::{Datum, PrimitiveType, Schema};
+use crate::scan::{
+    ArrowRecordBatchStream, FileScanTask, FileScanTaskStream,
+};
+use crate::spec::{
+    Datum, PrimitiveType, Schema,
+};
 use crate::utils::available_parallelism;
 use crate::{Error, ErrorKind};
 
@@ -734,10 +738,14 @@ impl PredicateConverter<'_> {
             let index = self
                 .column_indices
                 .iter()
-                .position(|&idx| idx == *column_idx).ok_or(Error::new(ErrorKind::DataInvalid, format!(
-                    "Leave column `{}` in predicates cannot be found in the required column indices.",
-                    reference.field().name
-                )))?;
+                .position(|&idx| idx == *column_idx)
+                .ok_or(Error::new(
+                    ErrorKind::DataInvalid,
+                    format!(
+                "Leave column `{}` in predicates cannot be found in the required column indices.",
+                reference.field().name
+            ),
+                ))?;
 
             Ok(Some(index))
         } else {
