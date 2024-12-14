0ba444fc06241123f78eabca4965aa72fbeb469e
feniljain <49019259+feniljain@users.noreply.github.com>
2024-12-14 15:57:17 +0530

    fix: set key_metadata to Null by default (#800)
    * fix: set key_metadata to Null by default
    
    * fix: return Option<&[u8]> instead of &Option<Vec<u8>> for key_metadata
    
    * test: use `None` instead of `Some` for key_metadata fields
    
    * refactor: use as_deref instead of explicit ref/deref using map

--- crates/iceberg/src/scan.rs
+++ crates/iceberg/src/scan.rs
@@ -1073,6 +1073,7 @@ mod tests {
                                 .file_size_in_bytes(100)
                                 .record_count(1)
                                 .partition(Struct::from_iter([Some(Literal::long(100))]))
+                                .key_metadata(None)
                                 .build()
                                 .unwrap(),
                         )
