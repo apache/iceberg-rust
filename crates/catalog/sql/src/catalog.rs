821f8dda3c4ebb635994e6acb8ed7914452f235f
Christian <christian@hansetag.com>
2024-12-15 16:26:22 +0100

    feat!: Remove `BoundPartitionSpec` (#771)
    * Remove bound partition spec
    
    * Fix UnboundPartitionSpec name
    
    * Update crates/iceberg/src/expr/visitors/expression_evaluator.rs
    
    Co-authored-by: Fokko Driesprong <fokko@apache.org>
    
    * Update crates/iceberg/src/spec/table_metadata.rs
    
    Co-authored-by: Fokko Driesprong <fokko@apache.org>
    
    * Fix syntax
    
    * Address comments
    
    ---------
    
    Co-authored-by: Fokko Driesprong <fokko@apache.org>

--- crates/catalog/sql/src/catalog.rs
+++ crates/catalog/sql/src/catalog.rs
@@ -783,7 +783,7 @@ mod tests {
     use std::hash::Hash;
 
     use iceberg::io::FileIOBuilder;
-    use iceberg::spec::{BoundPartitionSpec, NestedField, PrimitiveType, Schema, SortOrder, Type};
+    use iceberg::spec::{NestedField, PartitionSpec, PrimitiveType, Schema, SortOrder, Type};
     use iceberg::table::Table;
     use iceberg::{Catalog, Namespace, NamespaceIdent, TableCreation, TableIdent};
     use itertools::Itertools;
@@ -876,11 +876,10 @@ mod tests {
 
         assert_eq!(metadata.current_schema().as_ref(), expected_schema);
 
-        let expected_partition_spec = BoundPartitionSpec::builder(expected_schema.clone())
+        let expected_partition_spec = PartitionSpec::builder(expected_schema.clone())
             .with_spec_id(0)
             .build()
-            .unwrap()
-            .into_schemaless();
+            .unwrap();
 
         assert_eq!(
             metadata
