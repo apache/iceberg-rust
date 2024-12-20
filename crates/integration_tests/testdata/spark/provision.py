0777fa7349cd04dbe2a64805d0505e6465f94ad6
Fokko Driesprong <fokko@apache.org>
2024-12-20 08:21:51 +0100

    fix: Reading a table with positional deletes should fail (#826)
    * A table with positional deletes shoulds fail
    
    * Add possible fix
    
    * Comment and refactor

--- crates/integration_tests/testdata/spark/provision.py
+++ crates/integration_tests/testdata/spark/provision.py
@@ -18,7 +18,17 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import current_date, date_add, expr
 
-spark = SparkSession.builder.getOrCreate()
+# The configuration is important, otherwise we get many small
+# parquet files with a single row. When a positional delete
+# hits the Parquet file with one row, the parquet file gets
+# dropped instead of having a merge-on-read delete file.
+spark = (
+    SparkSession
+        .builder
+        .config("spark.sql.shuffle.partitions", "1")
+        .config("spark.default.parallelism", "1")
+        .getOrCreate()
+)
 
 spark.sql(
     f"""
