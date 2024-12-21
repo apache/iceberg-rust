f4f841e412de76919aad0a0934e9a5320c64cc66
Scott Donnelly <scott@donnel.ly>
2024-12-21 20:46:06 +0000

    refactor: introduce DeleteFileContext.
    Used to encapsulate enough information for DeleteFileIndex to be able to perform the requrired filtering of delete files

--- crates/iceberg/src/delete_file_index.rs
+++ crates/iceberg/src/delete_file_index.rs
@@ -2,11 +2,11 @@ use std::collections::HashMap;
 use std::sync::Arc;
 
 use futures::channel::mpsc;
-use futures::{StreamExt, TryStreamExt};
+use futures::{StreamExt, TryFutureExt, TryStreamExt};
 use tokio::sync::watch;
 
-use crate::scan::FileScanTaskDeleteFile;
-use crate::spec::{DataContentType, DataFile};
+use crate::scan::{DeleteFileContext, FileScanTaskDeleteFile};
+use crate::spec::{DataContentType, DataFile, Struct};
 use crate::Result;
 
 type DeleteFileIndexRef = Arc<Result<DeleteFileIndex>>;
@@ -16,17 +16,15 @@ pub(crate) type DeleteFileIndexRefReceiver = watch::Receiver<Option<DeleteFileIn
 #[derive(Debug)]
 pub(crate) struct DeleteFileIndex {
     #[allow(dead_code)]
-    global_deletes: Vec<Arc<FileScanTaskDeleteFile>>,
-    #[allow(dead_code)]
-    equality_deletes_by_partition: HashMap<i32, Vec<Arc<FileScanTaskDeleteFile>>>,
-    #[allow(dead_code)]
-    positional_deletes_by_partition: HashMap<i32, Vec<Arc<FileScanTaskDeleteFile>>>,
-    positional_deletes_by_path: HashMap<String, Vec<Arc<FileScanTaskDeleteFile>>>,
+    global_deletes: Vec<Arc<DeleteFileContext>>,
+    eq_deletes_by_partition: HashMap<Struct, Vec<Arc<DeleteFileContext>>>,
+    pos_deletes_by_partition: HashMap<Struct, Vec<Arc<DeleteFileContext>>>,
+    pos_deletes_by_path: HashMap<String, Vec<Arc<DeleteFileContext>>>,
 }
 
 impl DeleteFileIndex {
-    pub(crate) fn from_receiver(
-        receiver: mpsc::Receiver<Result<FileScanTaskDeleteFile>>,
+    pub(crate) fn from_del_file_chan(
+        receiver: mpsc::Receiver<Result<DeleteFileContext>>,
     ) -> watch::Receiver<Option<DeleteFileIndexRef>> {
         let (tx, rx) = watch::channel(None);
 
@@ -41,43 +39,55 @@ impl DeleteFileIndex {
         rx
     }
 
-    fn from_delete_files(files: Vec<FileScanTaskDeleteFile>) -> Self {
-        let mut equality_deletes_by_partition: HashMap<i32, Vec<Arc<FileScanTaskDeleteFile>>> =
+    fn from_delete_files(files: Vec<DeleteFileContext>) -> Self {
+        let mut eq_deletes_by_partition: HashMap<Struct, Vec<Arc<DeleteFileContext>>> =
             HashMap::default();
-        let mut positional_deletes_by_partition: HashMap<i32, Vec<Arc<FileScanTaskDeleteFile>>> =
+        let mut pos_deletes_by_partition: HashMap<Struct, Vec<Arc<DeleteFileContext>>> =
             HashMap::default();
-        let mut positional_deletes_by_path: HashMap<String, Vec<Arc<FileScanTaskDeleteFile>>> =
+        let mut pos_deletes_by_path: HashMap<String, Vec<Arc<DeleteFileContext>>> =
             HashMap::default();
 
-        files.into_iter().for_each(|file| {
-            let arc_file = Arc::new(file);
-            match arc_file.file_type {
+        files.into_iter().for_each(|del_file_ctx| {
+            let arc_del_file_ctx = Arc::new(del_file_ctx);
+            match arc_del_file_ctx.manifest_entry.content_type() {
                 DataContentType::PositionDeletes => {
                     // TODO: implement logic from ContentFileUtil.referencedDataFile
                     // see https://github.com/apache/iceberg/blob/cdf748e8e5537f13d861aa4c617a51f3e11dc97c/core/src/main/java/org/apache/iceberg/util/ContentFileUtil.java#L54
                     let referenced_data_file_path = "TODO".to_string();
 
-                    positional_deletes_by_path
+                    pos_deletes_by_path
                         .entry(referenced_data_file_path)
                         .and_modify(|entry| {
-                            entry.push(arc_file.clone());
+                            entry.push(arc_del_file_ctx.clone());
                         })
-                        .or_insert(vec![arc_file.clone()]);
+                        .or_insert(vec![arc_del_file_ctx.clone()]);
 
-                    positional_deletes_by_partition
-                        .entry(arc_file.partition_spec_id)
+                    pos_deletes_by_partition
+                        .entry(
+                            arc_del_file_ctx
+                                .manifest_entry
+                                .data_file()
+                                .partition()
+                                .clone(),
+                        )
                         .and_modify(|entry| {
-                            entry.push(arc_file.clone());
+                            entry.push(arc_del_file_ctx.clone());
                         })
-                        .or_insert(vec![arc_file.clone()]);
+                        .or_insert(vec![arc_del_file_ctx.clone()]);
                 }
                 DataContentType::EqualityDeletes => {
-                    equality_deletes_by_partition
-                        .entry(arc_file.partition_spec_id)
+                    eq_deletes_by_partition
+                        .entry(
+                            arc_del_file_ctx
+                                .manifest_entry
+                                .data_file()
+                                .partition()
+                                .clone(),
+                        )
                         .and_modify(|entry| {
-                            entry.push(arc_file.clone());
+                            entry.push(arc_del_file_ctx.clone());
                         })
-                        .or_insert(vec![arc_file.clone()]);
+                        .or_insert(vec![arc_del_file_ctx.clone()]);
                 }
                 _ => unreachable!(),
             }
@@ -85,9 +95,9 @@ impl DeleteFileIndex {
 
         DeleteFileIndex {
             global_deletes: vec![],
-            equality_deletes_by_partition,
-            positional_deletes_by_partition,
-            positional_deletes_by_path,
+            eq_deletes_by_partition,
+            pos_deletes_by_partition,
+            pos_deletes_by_path,
         }
     }
 
@@ -95,15 +105,60 @@ impl DeleteFileIndex {
     pub(crate) fn get_deletes_for_data_file(
         &self,
         data_file: &DataFile,
+        seq_num: Option<i64>,
     ) -> Vec<FileScanTaskDeleteFile> {
         let mut results = vec![];
 
-        if let Some(positional_deletes) = self.positional_deletes_by_path.get(data_file.file_path())
-        {
-            results.extend(positional_deletes.iter().map(|i| i.as_ref().clone()))
+        if let Some(deletes) = self.pos_deletes_by_path.get(data_file.file_path()) {
+            deletes
+                .iter()
+                .filter(|&delete| {
+                    seq_num
+                        .map(|seq_num| delete.manifest_entry.sequence_number() > Some(seq_num))
+                        .unwrap_or_else(|| true)
+                })
+                .for_each(|delete| {
+                    results.push(FileScanTaskDeleteFile {
+                        file_path: delete.manifest_entry.file_path().to_string(),
+                        file_type: delete.manifest_entry.content_type(),
+                        partition_spec_id: delete.partition_spec_id,
+                    })
+                });
         }
 
-        // TODO: equality deletes
+        if let Some(deletes) = self.pos_deletes_by_partition.get(data_file.partition()) {
+            deletes
+                .iter()
+                .filter(|&delete| {
+                    seq_num
+                        .map(|seq_num| delete.manifest_entry.sequence_number() > Some(seq_num))
+                        .unwrap_or_else(|| true)
+                })
+                .for_each(|delete| {
+                    results.push(FileScanTaskDeleteFile {
+                        file_path: delete.manifest_entry.file_path().to_string(),
+                        file_type: delete.manifest_entry.content_type(),
+                        partition_spec_id: delete.partition_spec_id,
+                    })
+                });
+        }
+
+        if let Some(deletes) = self.eq_deletes_by_partition.get(data_file.partition()) {
+            deletes
+                .iter()
+                .filter(|&delete| {
+                    seq_num
+                        .map(|seq_num| delete.manifest_entry.sequence_number() > Some(seq_num))
+                        .unwrap_or_else(|| true)
+                })
+                .for_each(|delete| {
+                    results.push(FileScanTaskDeleteFile {
+                        file_path: delete.manifest_entry.file_path().to_string(),
+                        file_type: delete.manifest_entry.content_type(),
+                        partition_spec_id: delete.partition_spec_id,
+                    })
+                });
+        }
 
         results
     }
