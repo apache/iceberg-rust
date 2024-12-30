439c5290446c455b8a08f6d256d44adefff522a7
Willi Raschkowski <wraschkowski@palantir.com>
2024-12-30 18:00:11 +0100

    Rename 'metadata_scan' to 'metadata_table'

--- crates/iceberg/src/table.rs
+++ crates/iceberg/src/table.rs
@@ -22,7 +22,7 @@ use std::sync::Arc;
 use crate::arrow::ArrowReaderBuilder;
 use crate::io::object_cache::ObjectCache;
 use crate::io::FileIO;
-use crate::metadata_scan::MetadataTable;
+use crate::metadata_table::MetadataTable;
 use crate::scan::TableScanBuilder;
 use crate::spec::{TableMetadata, TableMetadataRef};
 use crate::{Error, ErrorKind, Result, TableIdent};
