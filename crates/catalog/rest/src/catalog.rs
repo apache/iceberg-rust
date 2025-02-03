3e8e45e38b3a25691bbf385c2f5966a59fd81de9
Jonathan Chen <chenleejonathan@gmail.com>
2025-02-03 14:32:14 -0500

    fix

--- crates/catalog/rest/src/catalog.rs
+++ crates/catalog/rest/src/catalog.rs
@@ -1337,19 +1337,19 @@ mod tests {
         );
         let summary = SnapshotSummary::new(
             Operation::Append,
-            HashMap::from_iter([
-                ("spark.app.id", "local-1646787004168"),
-                ("added-data-files", "1"),
-                ("added-records", "1"),
-                ("added-files-size", "697"),
-                ("changed-partition-count", "1"),
-                ("total-records", "1"),
-                ("total-files-size", "697"),
-                ("total-data-files", "1"),
-                ("total-delete-files", "0"),
-                ("total-position-deletes", "0"),
-                ("total-equality-deletes", "0")
-            ].iter().map(|p| (p.0.to_string(), p.1.to_string()))),
+            HashMap::from([
+                ("spark.app.id".to_string(), "local-1646787004168".to_string()),
+                ("added-data-files".to_string(), "1".to_string()),
+                ("added-records".to_string(), "1".to_string()),
+                ("added-files-size".to_string(), "697".to_string()),
+                ("changed-partition-count".to_string(), "1".to_string()),
+                ("total-records".to_string(), "1".to_string()),
+                ("total-files-size".to_string(), "697".to_string()),
+                ("total-data-files".to_string(), "1".to_string()),
+                ("total-delete-files".to_string(), "0".to_string()),
+                ("total-position-deletes".to_string(), "0".to_string()),
+                ("total-equality-deletes".to_string(), "0".to_string()),
+            ]),
         );
 
         let expected_snapshot = Snapshot::builder()
