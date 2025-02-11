4c4af42d95eed7756b6f4d41ada17022fb1fbbbb
Connor Tsui <connor.tsui20@gmail.com>
2025-02-11 15:12:22 -0500

    examples rewrite

--- crates/examples/src/rest_catalog_table.rs
+++ crates/examples/src/rest_catalog_table.rs
@@ -21,23 +21,36 @@ use iceberg::spec::{NestedField, PrimitiveType, Schema, Type};
 use iceberg::{Catalog, TableCreation, TableIdent};
 use iceberg_catalog_rest::{RestCatalog, RestCatalogConfig};
 
-/// This is a simple example that demonstrates how to create a table in a REST catalog and get it back.
-/// It requires a running instance of the iceberg-rest catalog for the port 8080.
-/// You can find how to run the iceberg-rest catalog in the official documentation.
+static REST_URI: &str = "http://localhost:8181";
+static NAMESPACE: &str = "default";
+static TABLE_NAME: &str = "t1";
+
+/// This is a simple example that demonstrates how to use [`RestCatalog`] to create tables.
+/// 
+/// The demo creates a table creates a table and then later retrieves the same table.
 ///
-/// [Quickstart](https://iceberg.apache.org/spark-quickstart/)
+/// A running instance of the iceberg-rest catalog on port 8181 is required. You can find how to run
+/// the iceberg-rest catalog with `docker compose` in the official
+/// [quickstart documentation]((https://iceberg.apache.org/spark-quickstart/)).
 #[tokio::main]
 async fn main() {
-    // Create catalog
+    // Create the REST iceberg catalog.
     let config = RestCatalogConfig::builder()
-        .uri("http://localhost:8181".to_string())
+        .uri(REST_URI.to_string())
         .build();
-
     let catalog = RestCatalog::new(config);
 
-    // ANCHOR: create_table
-    let table_id = TableIdent::from_strs(["default", "t1"]).unwrap();
+    // Create the table identifier.
+    let table_id = TableIdent::from_strs([NAMESPACE, TABLE_NAME]).unwrap();
+    // println!("{:?}", table_id);
 
+    // Drop the table if it already exists.
+    if catalog.table_exists(&table_id).await.unwrap() {
+        println!("Table {TABLE_NAME} already exists, dropping now.");
+        catalog.drop_table(&table_id).await.unwrap();
+    }
+
+    // Build the table schema.
     let table_schema = Schema::builder()
         .with_fields(vec![
             NestedField::optional(1, "foo", Type::Primitive(PrimitiveType::String)).into(),
@@ -49,26 +62,24 @@ async fn main() {
         .build()
         .unwrap();
 
-    // Create table
+    // Build the table creation parameters.
     let table_creation = TableCreation::builder()
         .name(table_id.name.clone())
         .schema(table_schema.clone())
         .properties(HashMap::from([("owner".to_string(), "testx".to_string())]))
         .build();
 
-    let table = catalog
+    // Create the table.
+    let created_table = catalog
         .create_table(&table_id.namespace, table_creation)
         .await
         .unwrap();
+    // println!(
+    //     "Table {TABLE_NAME} created: {:#?}",
+    //     created_table.metadata()
+    // );
 
-    println!("Table created: {:?}", table.metadata());
-    // ANCHOR_END: create_table
-
-    // ANCHOR: load_table
-    let table_created = catalog
-        .load_table(&TableIdent::from_strs(["default", "t1"]).unwrap())
-        .await
-        .unwrap();
-    println!("{:?}", table_created.metadata());
-    // ANCHOR_END: load_table
+    // Load the table back from the catalog.
+    let loaded_table = catalog.load_table(&table_id).await.unwrap();
+    // println!("Table {TABLE_NAME} loaded: {:#?}", loaded_table.metadata());
 }
