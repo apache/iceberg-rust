ab190ff1e56d20f137b1f23284b35c1f3a4e493e
liurenjie1024 <liurenjie1024@users.noreply.github.com>
2025-03-03 04:48:15 +0000

    deploy: 3f2e61e3de93941fff3127893297f446acf20d0e

---
+++ api/trait.impl/parquet/arrow/async_reader/trait.AsyncFileReader.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"iceberg":[["impl&lt;R: <a class=\"trait\" href=\"iceberg/io/trait.FileRead.html\" title=\"trait iceberg::io::FileRead\">FileRead</a>&gt; AsyncFileReader for <a class=\"struct\" href=\"iceberg/arrow/struct.ArrowFileReader.html\" title=\"struct iceberg::arrow::ArrowFileReader\">ArrowFileReader</a>&lt;R&gt;"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
