58d774968bb14c7ef2d9f45912b6e18febc3e442
Devdutt Shenoi <devdutt@outlook.in>
2025-03-10 14:45:18 +0530

    doc: run doc test

--- crates/iceberg/src/writer/mod.rs
+++ crates/iceberg/src/writer/mod.rs
@@ -26,23 +26,59 @@
 //! 2. IcebergWriter: Focus on the logical format of iceberg table. It will write the data using the FileWriter finally.
 //!
 //! # Simple example for data file writer:
-//! ```ignore
+//! ```rust
+//! # use std::{sync::Arc, collections::HashMap};
+//! #
+//! # use arrow_array::{Int32Array, RecordBatch, StringArray};
+//! # use arrow_schema::{DataType, Field, Schema};
+//! # use iceberg::{
+//! #       Catalog, NamespaceIdent, TableCreation, TableIdent, arrow::arrow_schema_to_schema,
+//! #       io::FileIOBuilder, spec::DataFileFormat,
+//! #       writer::{
+//! #           IcebergWriter, IcebergWriterBuilder, base_writer::data_file_writer::DataFileWriterBuilder,
+//! #           file_writer::{ParquetWriterBuilder, location_generator::{DefaultFileNameGenerator, DefaultLocationGenerator}},
+//! #       },     
+//! # };
+//! # use iceberg_catalog_memory::MemoryCatalog;
+//! # use parquet::{arrow::PARQUET_FIELD_ID_META_KEY, file::properties::WriterProperties};
+//! # use tempdir::TempDir;
+//! #
+//! # #[tokio::main]
+//! # async fn main() {
+//! # let tmp_dir = TempDir::new("test_writer").unwrap();
+//! # let arrow_schema = Arc::new(Schema::new(vec![
+//! #       Field::new("id", DataType::Int32, false).with_metadata(HashMap::from_iter([(PARQUET_FIELD_ID_META_KEY.to_owned(), "1".to_owned())])),
+//! #       Field::new("name", DataType::Utf8, false).with_metadata(HashMap::from_iter([(PARQUET_FIELD_ID_META_KEY.to_owned(), "2".to_owned())])),
+//! #  ]));
+//! # let file_io = FileIOBuilder::new_fs_io().build().unwrap();
+//! # let catalog = MemoryCatalog::new(file_io.clone(), None);
+//! # let namespace_name = "test";
+//! # let ns_ident = NamespaceIdent::new(namespace_name.to_owned());
+//! # catalog.create_namespace(&ns_ident, Default::default()).await.unwrap();
+//! # let table_id = TableIdent::from_strs([namespace_name, "example".into()]).unwrap();
+//! # let iceberg_schema = arrow_schema_to_schema(&arrow_schema).unwrap();
+//! # let table_creation = TableCreation::builder().name(table_id.name).schema(iceberg_schema.clone()).location(tmp_dir.path().display().to_string()).properties(HashMap::new()).build();
+//! # let table = catalog.create_table(&ns_ident, table_creation).await.unwrap();
+//! # let location_gen = DefaultLocationGenerator::new(table.metadata().clone()).unwrap();
+//! # let file_name_gen = DefaultFileNameGenerator::new("".into(), None, DataFileFormat::Parquet);
+//! # let record_batch = RecordBatch::new_empty(arrow_schema);
+//! # let schema = Arc::new(iceberg_schema);
 //! // Create a parquet file writer builder. The parameter can get from table.
 //! let file_writer_builder = ParquetWriterBuilder::new(
-//!    0,
 //!    WriterProperties::builder().build(),
 //!    schema,
 //!    file_io.clone(),
-//!    loccation_gen,
+//!    location_gen,
 //!    file_name_gen,
-//! )
+//! );
 //! // Create a data file writer using parquet file writer builder.
-//! let data_file_builder = DataFileBuilder::new(file_writer_builder);
+//! let data_file_builder = DataFileWriterBuilder::new(file_writer_builder, None);
 //! // Build the data file writer.
-//! let data_file_writer = data_file_builder.build().await.unwrap();
+//! let mut data_file_writer = data_file_builder.build().await.unwrap();
 //!
-//! data_file_writer.write(&record_batch).await.unwrap();
-//! let data_files = data_file_writer.flush().await.unwrap();
+//! data_file_writer.write(record_batch).await.unwrap();
+//! let data_files = data_file_writer.close().await.unwrap();
+//! # }
 //! ```
 
 pub mod base_writer;
