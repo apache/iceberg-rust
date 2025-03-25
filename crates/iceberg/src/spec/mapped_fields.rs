ea31484de996fdc08149a2851f854f2f736adf67
Jack Dockerty <jdockerty@influxdata.com>
2025-03-25 16:59:41 +0000

    fix: remove owned string usage in id func

--- crates/iceberg/src/spec/mapped_fields.rs
+++ crates/iceberg/src/spec/mapped_fields.rs
@@ -79,8 +79,8 @@ impl MappedFields {
     }
 
     /// Get a field, by name, returning its ID if it exists, otherwise `None`.
-    pub fn id(&self, field_name: String) -> Option<i32> {
-        self.name_to_id.get(&field_name).copied()
+    pub fn id(&self, field_name: &str) -> Option<i32> {
+        self.name_to_id.get(field_name).copied()
     }
 
     /// Get a field, by ID, returning the underlying [`MappedField`] if it exists,
@@ -129,16 +129,16 @@ mod test {
         );
 
         assert!(
-            mapped_fields.id("not_exist".to_string()).is_none(),
+            mapped_fields.id("not_exist").is_none(),
             "Field was not expected to exist in the collection"
         );
-        assert_eq!(mapped_fields.id("field_two".to_string()), Some(2));
+        assert_eq!(mapped_fields.id("field_two"), Some(2));
         assert_eq!(
-            mapped_fields.id("field_foo".to_string()),
+            mapped_fields.id("field_foo"),
             Some(2),
             "Field with another name shares the same ID of '2'"
         );
-        assert_eq!(mapped_fields.id("field_one".to_string()), Some(1));
+        assert_eq!(mapped_fields.id("field_one"), Some(1));
     }
 
     #[test]
