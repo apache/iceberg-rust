124f5bd55b4b2e03cabf35ca9de2f63156d74e28
Xuanwo <Xuanwo@users.noreply.github.com>
2025-04-12 16:37:20 +0000

    deploy: 85c68b948648252788afda5a1fc7eed8ae2fcf34

--- api/trait.impl/core/iter/traits/iterator/trait.Iterator.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    var implementors = Object.fromEntries([["iceberg",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/iterator/trait.Iterator.html\" title=\"trait core::iter::traits::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"iceberg/spec/struct.StructValueIntoIter.html\" title=\"struct iceberg::spec::StructValueIntoIter\">StructValueIntoIter</a>"]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":57,"fragment_lengths":[352]}
