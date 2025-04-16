42a92edc694b55ca8da89f23fcf9d2f7252fc4c1
Xuanwo <github@xuanwo.io>
2025-04-16 18:48:42 +0800

    rename to make it more clear
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- crates/iceberg/src/cache.rs
+++ crates/iceberg/src/cache.rs
@@ -21,29 +21,43 @@ use std::sync::Arc;
 
 use crate::spec::{Manifest, ManifestList};
 
-/// A trait for caching objects of type `K` and `V`.
-pub trait Cache<K, V>: Send + Sync {
+/// A trait for caching in-memory objects of given type.
+///
+/// # Notes
+///
+/// ObjectCache will store deeply nested objects, such as `Manifest`,
+/// which contains `Schema`. Please ensure that the cache stores the
+/// object in memory as-is, without attempting to serialize it, as
+/// serialization could be extremely expensive.
+pub trait ObjectCache<K, V>: Send + Sync {
     /// Gets an object from the cache by its key.
     fn get(&self, key: &K) -> Option<V>;
     /// Sets an object in the cache with the given key and value.
     fn set(&self, key: K, value: V);
 }
 
-/// A trait for caching different objects used by iceberg.
-pub trait CacheProvide: Send + Sync {
+/// A trait for caching different in-memory objects used by iceberg.
+///
+/// # Notes
+///
+/// ObjectCache will store deeply nested objects, such as `Manifest`,
+/// which contains `Schema`. Please ensure that the cache stores the
+/// object in memory as-is, without attempting to serialize it, as
+/// serialization could be extremely expensive.
+pub trait ObjectCacheProvide: Send + Sync {
     /// Gets a cache for manifests.
-    fn manifest_cache(&self) -> &dyn Cache<String, Arc<Manifest>>;
+    fn manifest_cache(&self) -> &dyn ObjectCache<String, Arc<Manifest>>;
     /// Gets a cache for manifest lists.
-    fn manifest_list_cache(&self) -> &dyn Cache<String, Arc<ManifestList>>;
+    fn manifest_list_cache(&self) -> &dyn ObjectCache<String, Arc<ManifestList>>;
 }
 
 /// CacheProvider is a type alias for a thread-safe reference-counted pointer to a CacheProvide trait object.
-pub type CacheProvider = Arc<dyn CacheProvide>;
+pub type ObjectCacheProvider = Arc<dyn ObjectCacheProvide>;
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    struct _TestDynCompatibleForCache(Arc<dyn Cache<String, Arc<Manifest>>>);
-    struct _TestDynCompatibleForCacheProvider(CacheProvider);
+    struct _TestDynCompatibleForObjectCache(Arc<dyn ObjectCache<String, Arc<Manifest>>>);
+    struct _TestDynCompatibleForObjectCacheProvider(ObjectCacheProvider);
 }
