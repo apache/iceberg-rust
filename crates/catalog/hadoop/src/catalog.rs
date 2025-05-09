b13a01dcadeaa95d1a9f21085467c05e337bf163
awol2005ex <awol2005ex@163.com>
2025-05-09 15:56:56 +0800

    feat:Add hadoop catalog mode(hdfs_native drop_namespace)

--- crates/catalog/hadoop/src/catalog.rs
+++ crates/catalog/hadoop/src/catalog.rs
@@ -677,10 +677,38 @@ impl Catalog for HadoopCatalog {
                     return Err(Error::new(ErrorKind::DataInvalid, "warehouse is required"));
                 }
             }
+        } else if self.hdfs_native_client.is_some() {
+            let hdfs_native_client = self.hdfs_native_client.as_ref().unwrap();
+            let default_fs =
+                self.config
+                    .properties
+                    .get(FS_DEFAULTFS)
+                    .ok_or(iceberg::Error::new(
+                        ErrorKind::DataInvalid,
+                        " fs.defaultFS is null",
+                    ))?;
+
+            match self.config.warehouse.clone() {
+                Some(warehouse_url) => {
+                    let prefix = format!(
+                        "{}/{}",
+                        &warehouse_url[default_fs.len()..].to_string(),
+                        namespace.join("/")
+                    );
+
+                    hdfs_native_client
+                        .delete(&prefix, true)
+                        .await
+                        .map_err(|e| iceberg::Error::new(ErrorKind::Unexpected, e.to_string()))?;
+                }
+                None => {
+                    return Err(Error::new(ErrorKind::DataInvalid, "warehouse is required"));
+                }
+            }
         } else {
             return Err(Error::new(
                 ErrorKind::DataInvalid,
-                "s3 client is not initialized",
+                "s3 client or hdfs native client is not initialized",
             ));
         }
 
