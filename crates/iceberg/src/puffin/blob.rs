32538ec29a8a8fa619b9b2c7e10cd751adbe6bc0
dentiny <dentinyhao@gmail.com>
2025-05-14 03:27:30 +0000

    use typed builder

--- crates/iceberg/src/puffin/blob.rs
+++ crates/iceberg/src/puffin/blob.rs
@@ -17,13 +17,15 @@
 
 use std::collections::HashMap;
 
+use typed_builder::TypedBuilder;
+
 /// A serialized form of a "compact" Theta sketch produced by the Apache DataSketches library.
 pub const APACHE_DATASKETCHES_THETA_V1: &str = "apache-datasketches-theta-v1";
 /// A serialized form of a deletion vector.
 pub const DELETION_VECTOR_V1: &str = "deletion-vector-v1";
 
 /// The blob
-#[derive(Debug, PartialEq, Clone)]
+#[derive(Debug, PartialEq, Clone, TypedBuilder)]
 pub struct Blob {
     pub(crate) r#type: String,
     pub(crate) fields: Vec<i32>,
@@ -34,11 +36,6 @@ pub struct Blob {
 }
 
 impl Blob {
-    /// Returns a BlobBuilder to build a build
-    pub fn builder() -> BlobBuilder {
-        BlobBuilder::default()
-    }
-
     #[inline]
     /// See blob types: https://iceberg.apache.org/puffin-spec/#blob-types
     pub fn blob_type(&self) -> &str {
@@ -73,59 +70,5 @@ impl Blob {
     /// Arbitrary meta-information about the blob
     pub fn properties(&self) -> &HashMap<String, String> {
         &self.properties
-    }
-}
-
-/// Builder to create a Blob.
-#[derive(Debug, Default, PartialEq, Clone)]
-pub struct BlobBuilder {
-    r#type: String,
-    fields: Vec<i32>,
-    snapshot_id: i64,
-    sequence_number: i64,
-    data: Vec<u8>,
-    properties: HashMap<String, String>,
-}
-
-impl BlobBuilder {
-    pub fn r#type(mut self, t: impl Into<String>) -> Self {
-        self.r#type = t.into();
-        self
-    }
-
-    pub fn fields(mut self, fields: Vec<i32>) -> Self {
-        self.fields = fields;
-        self
-    }
-
-    pub fn snapshot_id(mut self, id: i64) -> Self {
-        self.snapshot_id = id;
-        self
-    }
-
-    pub fn sequence_number(mut self, num: i64) -> Self {
-        self.sequence_number = num;
-        self
-    }
-
-    pub fn data(mut self, data: Vec<u8>) -> Self {
-        self.data = data;
-        self
-    }
-
-    pub fn properties(mut self, props: HashMap<String, String>) -> Self {
-        self.properties = props;
-        self
-    }
-
-    pub fn build(self) -> Blob {
-        Blob {
-            r#type: self.r#type,
-            fields: self.fields,
-            snapshot_id: self.snapshot_id,
-            sequence_number: self.sequence_number,
-            data: self.data,
-            properties: self.properties,
-        }
     }
 }
