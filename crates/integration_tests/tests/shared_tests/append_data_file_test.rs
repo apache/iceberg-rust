00b7b876db1f0c7b91d22bf54746a23a00da19a6
Roman Shanin <rshanin@bhft.com>
2025-05-30 19:46:29 +0300

    fix(iceberg): add_files correctly check duplicates

--- crates/integration_tests/tests/shared_tests/append_data_file_test.rs
+++ crates/integration_tests/tests/shared_tests/append_data_file_test.rs
@@ -129,25 +129,4 @@ async fn test_append_data_file() {
     let batches: Vec<_> = batch_stream.try_collect().await.unwrap();
     assert_eq!(batches.len(), 1);
     assert_eq!(batches[0], batch);
-
-    // commit result again
-    let tx = Transaction::new(&table);
-    let mut append_action = tx.fast_append(None, vec![]).unwrap();
-    append_action.add_data_files(data_file.clone()).unwrap();
-    let tx = append_action.apply().await.unwrap();
-    let table = tx.commit(&rest_catalog).await.unwrap();
-
-    // check result again
-    let batch_stream = table
-        .scan()
-        .select_all()
-        .build()
-        .unwrap()
-        .to_arrow()
-        .await
-        .unwrap();
-    let batches: Vec<_> = batch_stream.try_collect().await.unwrap();
-    assert_eq!(batches.len(), 2);
-    assert_eq!(batches[0], batch);
-    assert_eq!(batches[1], batch);
 }
