086dff7c55dbe91019fc50dddee81caf38146224
Shawn Chang <yxchang@amazon.com>
2025-06-18 04:42:36 -0700

    feat(transaction): Implement TransactionAction for FastAppendAction (#1448)
    ## Which issue does this PR close?
    
    Related Issues:
    - #1382 [EPIC]
    
    ## What changes are included in this PR?
    
    - Implement `TransactionAction` for `FastAppendAction`
    - Changed `SnapshotProduceAction` to `SnapshotProducer`
    - The idea is to make `SnapshotProduceAction` an _stateless_ helper
    class that helps with fast_append::commit (as discussed
    [here](https://github.com/apache/iceberg-rust/pull/1400#discussion_r2131851242))
    
    ## Are these changes tested?
    Added unit tests

--- crates/iceberg/src/transaction/mod.rs
+++ crates/iceberg/src/transaction/mod.rs
@@ -28,7 +28,6 @@ mod update_location;
 mod update_properties;
 mod upgrade_format_version;
 
-use std::collections::HashMap;
 use std::mem::discriminant;
 use std::sync::Arc;
 
@@ -142,19 +141,8 @@ impl Transaction {
     }
 
     /// Creates a fast append action.
-    pub fn fast_append(
-        self,
-        commit_uuid: Option<Uuid>,
-        key_metadata: Vec<u8>,
-    ) -> Result<FastAppendAction> {
-        let snapshot_id = self.generate_unique_snapshot_id();
-        FastAppendAction::new(
-            self,
-            snapshot_id,
-            commit_uuid.unwrap_or_else(Uuid::now_v7),
-            key_metadata,
-            HashMap::new(),
-        )
+    pub fn fast_append(&self) -> FastAppendAction {
+        FastAppendAction::new(self.generate_unique_snapshot_id())
     }
 
     /// Creates replace sort order action.
