e4479deddedc4fa01dceb5fdf321a7ce1e7afa9a
Shawn Chang <yxchang@amazon.com>
2025-07-21 18:28:28 -0700

    minor

--- crates/catalog/rest/src/catalog.rs
+++ crates/catalog/rest/src/catalog.rs
@@ -794,7 +794,8 @@ impl Catalog for RestCatalog {
                 return Err(Error::new(
                     ErrorKind::CatalogCommitConflicts,
                     "CatalogCommitConflicts, one or more requirements failed. The client may retry.",
-                ).with_retryable(true));
+                )
+                .with_retryable(true));
             }
             StatusCode::INTERNAL_SERVER_ERROR => {
                 return Err(Error::new(
