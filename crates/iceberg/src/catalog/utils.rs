37c10604bfa7c30e1b23ca4a467e5ff084c533dc
Shawn Chang <yexiangchang@gmail.com>
2025-07-24 09:30:09 -0700

    Update crates/iceberg/src/catalog/utils.rs
    Co-authored-by: Renjie Liu <liurenjie2008@gmail.com>

--- crates/iceberg/src/catalog/utils.rs
+++ crates/iceberg/src/catalog/utils.rs
@@ -24,7 +24,7 @@ use crate::{Error, ErrorKind, Result};
 
 /// Helper for parsing a location of the format: `<prefix>/metadata/<version>-<uuid>.metadata.json`
 #[derive(Clone, Debug, PartialEq)]
-pub struct MetadataLocationParser {
+pub struct MetadataLocation {
     prefix: String,
     version: i32,
     id: Uuid,
