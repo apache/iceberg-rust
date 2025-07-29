524d71636fb23fe0bbd22e96544a17d9a251f30e
liurenjie1024 <liurenjie1024@users.noreply.github.com>
2025-07-29 09:54:39 +0000

    deploy: 3ab45eec78145715eddf21c1e1758103b7f34914

--- api/iceberg/writer/file_writer/sidebar-items.js
+++ api/iceberg/writer/file_writer/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":["location_generator"],"struct":["ParquetWriter","ParquetWriterBuilder"],"trait":["FileWriter","FileWriterBuilder"]};
+window.SIDEBAR_ITEMS = {"mod":["location_generator","rolling_writer"],"struct":["ParquetWriter","ParquetWriterBuilder"],"trait":["FileWriter","FileWriterBuilder"]};
