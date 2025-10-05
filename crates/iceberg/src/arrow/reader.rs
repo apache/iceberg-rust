b121d8333ee23479c98bff6c76f20912328440c6
DerGut <jannik.steinmann@gmx.de>
2025-10-05 17:32:11 +0200

    Resolve conflicts

--- crates/iceberg/src/arrow/reader.rs
+++ crates/iceberg/src/arrow/reader.rs
@@ -39,7 +39,9 @@ use parquet::arrow::arrow_reader::{
 };
 use parquet::arrow::async_reader::AsyncFileReader;
 use parquet::arrow::{PARQUET_FIELD_ID_META_KEY, ParquetRecordBatchStreamBuilder, ProjectionMask};
-use parquet::file::metadata::{ParquetMetaData, ParquetMetaDataReader, RowGroupMetaData};
+use parquet::file::metadata::{
+    PageIndexPolicy, ParquetMetaData, ParquetMetaDataReader, RowGroupMetaData,
+};
 use parquet::schema::types::{SchemaDescriptor, Type as ParquetType};
 
 use crate::arrow::caching_delete_file_loader::CachingDeleteFileLoader;
@@ -1390,9 +1392,9 @@ impl<R: FileRead> AsyncFileReader for ArrowFileReader<R> {
         async move {
             let reader = ParquetMetaDataReader::new()
                 .with_prefetch_hint(self.metadata_size_hint)
-                .with_column_indexes(self.preload_column_index)
-                .with_page_indexes(self.preload_page_index)
-                .with_offset_indexes(self.preload_offset_index);
+                .with_column_index_policy(PageIndexPolicy::from(self.preload_column_index))
+                .with_page_index_policy(PageIndexPolicy::from(self.preload_page_index))
+                .with_offset_index_policy(PageIndexPolicy::from(self.preload_offset_index));
             let size = self.meta.size;
             let meta = reader.load_and_finish(self, size).await?;
 
