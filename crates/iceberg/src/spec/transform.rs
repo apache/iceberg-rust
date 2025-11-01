235cf50ca25a52d0331189524b3ed313e6526560
Junwang Zhao <zhjwpku@gmail.com>
2025-11-01 21:38:43 +0800

    fix: result_type of Day Transform should be Int per spec
    Returning Date for Transform::Day in Transform::result_type doesn't
    cause any issues, but it's better to follow the spec.
    
    Signed-off-by: Junwang Zhao <zhjwpku@gmail.com>

--- crates/iceberg/src/spec/transform.rs
+++ crates/iceberg/src/spec/transform.rs
@@ -218,7 +218,7 @@ impl Transform {
                     ))
                 }
             }
-            Transform::Year | Transform::Month => {
+            Transform::Year | Transform::Month | Transform::Day => {
                 if let Type::Primitive(p) = input_type {
                     match p {
                         PrimitiveType::Timestamp
@@ -226,26 +226,6 @@ impl Transform {
                         | PrimitiveType::TimestampNs
                         | PrimitiveType::TimestamptzNs
                         | PrimitiveType::Date => Ok(Type::Primitive(PrimitiveType::Int)),
-                        _ => Err(Error::new(
-                            ErrorKind::DataInvalid,
-                            format!("{input_type} is not a valid input type of {self} transform",),
-                        )),
-                    }
-                } else {
-                    Err(Error::new(
-                        ErrorKind::DataInvalid,
-                        format!("{input_type} is not a valid input type of {self} transform",),
-                    ))
-                }
-            }
-            Transform::Day => {
-                if let Type::Primitive(p) = input_type {
-                    match p {
-                        PrimitiveType::Timestamp
-                        | PrimitiveType::Timestamptz
-                        | PrimitiveType::TimestampNs
-                        | PrimitiveType::TimestamptzNs
-                        | PrimitiveType::Date => Ok(Type::Primitive(PrimitiveType::Date)),
                         _ => Err(Error::new(
                             ErrorKind::DataInvalid,
                             format!("{input_type} is not a valid input type of {self} transform",),
