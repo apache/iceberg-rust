c57e9dae28edfaeb71f6fc4fbb18774a1b15658d
blackmwk <blackmwk@users.noreply.github.com>
2026-02-03 00:55:37 +0000

    deploy: 0c3e5f0ed7e00857d751581f46c787e13f2298cf

--- api/trait.impl/reqsign/aws/credential/trait.CredentialLoad.js
+++ api/trait.impl/reqsign/aws/credential/trait.CredentialLoad.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["iceberg",[["impl <a class=\"trait\" href=\"iceberg/io/trait.AwsCredentialLoad.html\" title=\"trait iceberg::io::AwsCredentialLoad\">CredentialLoad</a> for <a class=\"struct\" href=\"iceberg/io/struct.CustomAwsCredentialLoader.html\" title=\"struct iceberg::io::CustomAwsCredentialLoader\">CustomAwsCredentialLoader</a>"]]]]);
+    var implementors = Object.fromEntries([["iceberg",[["impl CredentialLoad for <a class=\"struct\" href=\"iceberg/io/struct.CustomAwsCredentialLoader.html\" title=\"struct iceberg::io::CustomAwsCredentialLoader\">CustomAwsCredentialLoader</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[324]}
+//{"start":57,"fragment_lengths":[205]}
