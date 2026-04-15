3fe7be196cae9e119747de3bc3fcae5818e2e9b0
blackmwk <liurenjie1024@outlook.com>
2026-04-16 01:30:19 +0800

    Fix RUSTSEC-2026-0097 (#2331)
    ## Which issue does this PR close?
    
    
    - Closes #2327 #2328 #2329
    
    ## What changes are included in this PR?
    
    Upgrade rnd version, use recommended api.
    
    ## Are these changes tested?
    
    ut.

--- crates/iceberg/src/expr/visitors/page_index_evaluator.rs
+++ crates/iceberg/src/expr/visitors/page_index_evaluator.rs
@@ -793,7 +793,7 @@ mod tests {
     };
     use parquet::file::metadata::{PageIndexPolicy, ParquetMetaData};
     use parquet::file::properties::WriterProperties;
-    use rand::{Rng, thread_rng};
+    use rand::Rng;
     use tempfile::NamedTempFile;
 
     use super::PageIndexEvaluator;
@@ -1284,13 +1284,13 @@ mod tests {
 
     #[test]
     fn eval_in_length_of_set_above_limit_all_rows() -> Result<()> {
-        let mut rng = thread_rng();
+        let mut rng = rand::rng();
         let (metadata, _temp_file) = create_test_parquet_file()?;
         let (column_index, offset_index, row_group_metadata) = get_test_metadata(&metadata);
         let (iceberg_schema_ref, field_id_map) = build_iceberg_schema_and_field_map()?;
 
         let filter = Reference::new("col_float")
-            .is_in(std::iter::repeat_with(|| Datum::float(rng.gen_range(0.0..10.0))).take(1000))
+            .is_in(std::iter::repeat_with(|| Datum::float(rng.random_range(0.0..10.0))).take(1000))
             .bind(iceberg_schema_ref.clone(), false)?;
 
         let result = PageIndexEvaluator::eval(
